fc7b2641868ce35494716c252e7f5e879e443830
jinifor <jinifor@gmail.com>
2014-02-13 12:31:49 +0900

    add sample file

---
+++ 02_Intensive/DeleteFeature.js
@@ -0,0 +1,21 @@
+﻿OpenLayers.Control.DeleteFeature = OpenLayers.Class(OpenLayers.Control, {
+	layer: null,
+	initialize: function( layer, options ) {
+		OpenLayers.Control.prototype.initialize.apply(this, [options]);
+		this.layer = layer;
+		this.handler = new OpenLayers.Handler.Feature(
+			this, this.layer, { click: this.clickFeature }
+		);
+	},
+	clickFeature: function(feature) {
+		if (feature.fid == undefined) {
+			this.layer.destroyFeatures([feature]);
+		} else {
+			feature.state = OpenLayers.State.DELETE;
+			this.layer.events.triggerEvent("afterfeaturemodified", {feature: feature});
+			feature.renderIntent = "select";
+			this.layer.drawFeature(feature);
+		}
+	},
+	CLASS_NAME: "OpenLayers.Control.DeleteFeature"
+});
